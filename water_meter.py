429d5df333d22689ba8ee40fecbb1fdb94ca4bb9
Patrik Horemans <horemansp@hotmail.com>
2022-09-17 15:33:04 +0200

    Update water_meter.py

--- water_meter.py
+++ water_meter.py
@@ -1,21 +1,24 @@
 import RPi.GPIO as GPIO
-import time
 from datetime import datetime
 
 REED_PIN = 12
 GPIO.setmode(GPIO.BCM)
 WATER_COUNTER = 0.0
+WATER_10L = 0.0
 
 def reed_contact(channel):
     global WATER_COUNTER
+    global WATER_10L
     WATER_COUNTER = WATER_COUNTER + 0.5
-    print(datetime.now()," Verbruikt liter: ",WATER_COUNTER)
+    WATER_10L = WATER_10L + 0.5
+    print(datetime.now()," Water consumed: ",WATER_COUNTER)
     
 GPIO.setup(REED_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(REED_PIN, GPIO.FALLING, callback=reed_contact, bouncetime=50)
 
 while True:
-    if WATER_COUNTER > 10:
-        print("10 liter verbruikt!")
+    if WATER_10L > 10:
+        WATER_10L = 0
+        print("10 liters consumed!")
 
 GPIO.cleanup()
